d9d47a20bcd37cd786b1bc4b453a74d9e79538c1
amjadsh92 <amjadsharafeddien@gmail.com>
2025-05-14 21:42:21 +0300

    fix: Polish the project

--- number_guess.sh
+++ number_guess.sh
@@ -8,6 +8,7 @@ USER_NAME_EXIST=$($PSQL "SELECT user_id FROM users WHERE name='$USER_NAME' ")
 if [[ -z $USER_NAME_EXIST ]]
 then
 USER_INSERTED=$($PSQL "INSERT INTO users(name) VALUES('$USER_NAME')")
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE name='$USER_NAME'")
 echo "Welcome, $USER_NAME! It looks like this is your first time here."
 else
 USER_ID=$($PSQL "SELECT user_id FROM users WHERE name='$USER_NAME'")
@@ -27,17 +28,16 @@ echo "That is not an integer, guess again:"
 ((NUMBER_OF_GUESSES++)) 
 elif (( NUMBER_GUESSED > SECRET_NUMBER ))
 then
-echo "It's higher than that, guess again:"
+echo "It's lower than that, guess again:"
 ((NUMBER_OF_GUESSES++))
 elif (( NUMBER_GUESSED < SECRET_NUMBER ))
 then
-echo "It's lower than that, guess again:"
+echo "It's higher than that, guess again:"
 ((NUMBER_OF_GUESSES++))
 elif (( NUMBER_GUESSED == SECRET_NUMBER ))
 then
-echo "YOU GOT IT!"
 ((NUMBER_OF_GUESSES++))
-USER_ID=$($PSQL "SELECT user_id FROM users WHERE name='$USER_NAME'")
+echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
 GAME_SUBMITED=$($PSQL "INSERT INTO games(user_id,number_of_guesses) VALUES('$USER_ID',$NUMBER_OF_GUESSES)") 
 fi
 done 
