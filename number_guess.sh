3002b998198947b8b2872bdc9fbd058106c0f4b1
amjadsh92 <amjadsharafeddien@gmail.com>
2025-05-14 19:41:17 +0300

    feat: Insert a finished game into the games table in the database

--- number_guess.sh
+++ number_guess.sh
@@ -12,7 +12,7 @@ echo "Welcome, $USER_NAME! It looks like this is your first time here."
 else
 echo "Welcome back, $USER_NAME!"
 fi
-SECRET_NUMBER=$(( RANDOM % 100 + 1 ))
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
 NUMBER_OF_GUESSES=0
 echo "Guess the secret number between 1 and 1000:"
 while [[ ! $NUMBER_GUESSED =~ ^-?[0-9]+$ || $NUMBER_GUESSED -ne $SECRET_NUMBER ]]
@@ -33,7 +33,9 @@ echo "It's lower than that, guess again:"
 elif (( NUMBER_GUESSED == SECRET_NUMBER ))
 then
 echo "YOU GOT IT!"
-((NUMBER_OF_GUESSES++)) 
+((NUMBER_OF_GUESSES++))
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE name='$USER_NAME'")
+GAME_SUBMITED=$($PSQL "INSERT INTO games(user_id,number_of_guesses) VALUES('$USER_ID',$NUMBER_OF_GUESSES)") 
 fi
 done 
 
