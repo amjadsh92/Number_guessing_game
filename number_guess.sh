6915ace1934646f4697ee7d5093c360d13f83ee4
amjadsh92 <amjadsharafeddien@gmail.com>
2025-05-14 18:34:37 +0300

    feat: Track the number of guesses in the game

--- number_guess.sh
+++ number_guess.sh
@@ -13,22 +13,27 @@ else
 echo "Welcome back, $USER_NAME!"
 fi
 SECRET_NUMBER=$(( RANDOM % 100 + 1 ))
+NUMBER_OF_GUESSES=0
 echo "Guess the secret number between 1 and 1000:"
-while [[ $NUMBER_GUESSED -ne $SECRET_NUMBER ]]
+while [[ ! $NUMBER_GUESSED =~ ^-?[0-9]+$ || $NUMBER_GUESSED -ne $SECRET_NUMBER ]]
 do
 read NUMBER_GUESSED
 if [[ ! $NUMBER_GUESSED =~ ^-?[0-9]+$ ]]
 then
 echo "That is not an integer, guess again:"
-elif (( NUMBER_GUESSED > SECRET_NUMBER )) 
+((NUMBER_OF_GUESSES++)) 
+elif (( NUMBER_GUESSED > SECRET_NUMBER ))
 then
 echo "It's higher than that, guess again:"
+((NUMBER_OF_GUESSES++))
 elif (( NUMBER_GUESSED < SECRET_NUMBER ))
 then
 echo "It's lower than that, guess again:"
+((NUMBER_OF_GUESSES++))
 elif (( NUMBER_GUESSED == SECRET_NUMBER ))
 then
 echo "YOU GOT IT!"
+((NUMBER_OF_GUESSES++)) 
 fi
 done 
 
