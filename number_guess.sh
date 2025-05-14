1c3a7e41c00bbfbdb03b77a8b679a73bc627413d
amjadsh92 <amjadsharafeddien@gmail.com>
2025-05-14 15:32:19 +0300

    test: Ask about an input from the user

--- number_guess.sh
+++ number_guess.sh
@@ -1,2 +1,11 @@
 #! /bin/bash
 
+PSQL="psql --username=amjad --dbname=number_guess_name -t --no-align -c"
+
+echo "Enter your username:" 
+read USER_NAME
+echo "Welcome, $USER_NAME! It looks like this is your first time here."
+echo "Guess the secret number between 1 and 1000:"
+read NUMBER_GUESSED
+ 
+
